48d927ee818a9dc3677c707dae1f7858a4afe054
David <davidshevnya@ukr.net>
2026-01-11 22:09:45 +0000

    Refactor code

--- app/routes/users.py
+++ app/routes/users.py
@@ -9,21 +9,21 @@ users_bp = Blueprint('users', __name__, url_prefix='/users')
 @users_bp.route('/<int:id>', methods=['GET'])
 @jwt_required()
 def get_user(id):
-    user = User.query.get_or_404(id)
+    user = db.session.get(User, id)
     return jsonify(user.to_json())
 
 @users_bp.route('/me', methods=['GET'])
 @jwt_required()
 def get_current_user():
     current_user_id = get_jwt_identity()
-    user = User.query.get_or_404(current_user_id)
+    user = db.get_or_404(User, current_user_id)
     return jsonify(user.to_json(include_email=True))
 
 @users_bp.route('/me/edit', methods=['PUT'])
 @jwt_required()
 def edit_current_user():
     current_user_id = get_jwt_identity()
-    user = User.query.get_or_404(current_user_id)
+    user = db.get_or_404(User, current_user_id)
     
     data = request.get_json()
     allowed_fields = [
